2aaca1fafb3c726455e1e91d93a739bb91f7327a
Tashin Ahmed (তাশীন আহমেদ) <tahmed153109@bscse.uiu.ac.bd>
2019-09-07 18:43:21 +0600

    Add files via upload

---
+++ tetrisCPP/cmake-build-debug/CMakeFiles/tetrisCPP.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tetrisCPP.dir/main.cpp.obj"
+  "tetrisCPP.pdb"
+  "tetrisCPP.exe"
+  "tetrisCPP.exe.manifest"
+  "libtetrisCPP.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tetrisCPP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
